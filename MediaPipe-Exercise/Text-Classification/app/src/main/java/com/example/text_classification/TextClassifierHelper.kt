1be6379aff43f9af064036287993327ac94ae33c
ridzky rahim <ridzkyrhm37@gmail.com>
2024-04-18 12:16:09 +0700

    added function classify

--- MediaPipe-Exercise/Text-Classification/app/src/main/java/com/example/text_classification/TextClassifierHelper.kt
+++ MediaPipe-Exercise/Text-Classification/app/src/main/java/com/example/text_classification/TextClassifierHelper.kt
@@ -1,17 +1,20 @@
 package com.example.text_classification
 
 import android.content.Context
+import android.os.SystemClock
 import android.util.Log
 import com.google.mediapipe.tasks.components.containers.Classifications
 import com.google.mediapipe.tasks.core.BaseOptions
 import com.google.mediapipe.tasks.text.textclassifier.TextClassifier
+import java.util.concurrent.ScheduledThreadPoolExecutor
 
 class TextClassifierHelper(
     val modelName: String = "bert_classifier.tflite",
     val context: Context,
     val classifierListener: ClassifierListener? = null,
 ) {
-    private val textClassifier: TextClassifier? = null
+    private var textClassifier: TextClassifier? = null
+    private var executor: ScheduledThreadPoolExecutor? = null
 
     init {
         initClassifier()
@@ -28,6 +31,22 @@ class TextClassifierHelper(
         } catch (e: IllegalStateException) {
             classifierListener?.onError(context.getString(R.string.text_classifier_failed))
             Log.e(TAG, e.message.toString())
+        }
+    }
+
+    fun classify(inputText: String){
+        if (textClassifier == null){
+            initClassifier()
+        }
+
+        executor = ScheduledThreadPoolExecutor(1)
+
+        executor?.execute{
+            var inferenceTime = SystemClock.uptimeMillis()
+            val result = textClassifier?.classify(inputText)
+            inferenceTime = SystemClock.uptimeMillis() - inferenceTime
+
+            classifierListener?.onResult(result?.classificationResult()?.classifications(),inferenceTime)
         }
     }
 
