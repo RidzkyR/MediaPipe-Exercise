65b74d10161083493f1c980c5f0acbdd67c37096
ridzky rahim <ridzkyrhm37@gmail.com>
2024-04-18 12:03:30 +0700

    added textclassifier configuration

--- MediaPipe-Exercise/Text-Classification/app/src/main/java/com/example/text_classification/TextClassifierHelper.kt
+++ MediaPipe-Exercise/Text-Classification/app/src/main/java/com/example/text_classification/TextClassifierHelper.kt
@@ -1,18 +1,45 @@
 package com.example.text_classification
 
 import android.content.Context
+import android.util.Log
 import com.google.mediapipe.tasks.components.containers.Classifications
+import com.google.mediapipe.tasks.core.BaseOptions
+import com.google.mediapipe.tasks.text.textclassifier.TextClassifier
 
 class TextClassifierHelper(
     val modelName: String = "bert_classifier.tflite",
     val context: Context,
     val classifierListener: ClassifierListener? = null,
 ) {
+    private val textClassifier: TextClassifier? = null
+
+    init {
+        initClassifier()
+    }
+
+    private fun initClassifier() {
+        try {
+            // Konfigurasi TextClassifier MediaPipe
+            val optionBuilder = TextClassifier.TextClassifierOptions.builder()
+
+            val baseOptionBuilder = BaseOptions.builder().setModelAssetPath(modelName)
+            optionBuilder.setBaseOptions(baseOptionBuilder.build())
+
+        } catch (e: IllegalStateException) {
+            classifierListener?.onError(context.getString(R.string.text_classifier_failed))
+            Log.e(TAG, e.message.toString())
+        }
+    }
+
     interface ClassifierListener {
         fun onError(error: String)
         fun onResult(
             result: List<Classifications>?,
-            inferenceTime: Long
+            inferenceTime: Long,
         )
+    }
+
+    companion object {
+        private const val TAG = "TextClassifierHelper"
     }
 }
